5cd4e1d71f9281e5771d454a9c197a331c4f8284
root <lptnii@qq.com>
2018-09-27 00:08:35 +0800

    sum

--- java-box-api-service/src/main/java/com/wzy/action/BoxUserController.java
+++ java-box-api-service/src/main/java/com/wzy/action/BoxUserController.java
@@ -1,8 +1,18 @@
 package com.wzy.action;
 
+import com.wzy.action.parameter.user.Login;
+import com.wzy.util.annotation.factory.Verification;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BoxUserController {
 
+    @RequestMapping(value = "/login")
+    public String login(Login login) {
+        return Verification.verification(login).setBusiness(jsonVo -> {
+
+            return jsonVo;
+        }).init().returnJsonString();
+    }
 }
---
+++ java-box-api-service/src/main/java/com/wzy/action/parameter/user/Login.java
@@ -0,0 +1,29 @@
+package com.wzy.action.parameter.user;
+
+import com.wzy.util.annotation.check.IsContrastString;
+import com.wzy.util.annotation.check.IsNotNull;
+import com.wzy.util.annotation.set.SetStringMd5;
+
+public class Login {
+    @IsNotNull(message = "密码不能为空")
+    @SetStringMd5
+    String user_pass;
+    @IsNotNull(message = "账号不能为空")
+    String user_name;
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getUser_pass() {
+        return user_pass;
+    }
+
+    public void setUser_pass(String user_pass) {
+        this.user_pass = user_pass;
+    }
+}
