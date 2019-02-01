47d4dada043b260ce041a54c1236cc199680286c
root <lptnii@qq.com>
2019-02-02 01:21:07 +0800

    配置中心

--- java-box-plug/src/main/java/com/wzy/plug/db/druid/DruidUtil.java
+++ java-box-plug/src/main/java/com/wzy/plug/db/druid/DruidUtil.java
@@ -1,30 +1,41 @@
 package com.wzy.plug.db.druid;
 import com.alibaba.druid.pool.DruidDataSource;
+import com.wzy.func.annotation.BoxInit;
 import com.wzy.func.fc.IBoxDataSource;
-
+import com.wzy.func.fc.IConfig;
+import com.wzy.log.BoxLog;
+import com.wzy.log.ILog;
 import javax.sql.DataSource;
-public class DruidUtil implements IBoxDataSource {
-    private String url;
-    private String username;
-    private String password;
-    private String driverClassName;
-    private int initialSize;
-    private int minIdle;
-    private int maxActive;
-    private int maxWait;
-    private int timeBetweenEvictionRunsMillis;
-    private int minEvictableIdleTimeMillis;
-    private String validationQuery;
-    private boolean testWhileIdle;
-    private boolean testOnBorrow;
-    private boolean testOnReturn;
-    private boolean poolPreparedStatements;
-    private int maxPoolPreparedStatementPerConnectionSize;
-    private String filters;
-    private String connectionProperties;
 
+public class DruidUtil implements IBoxDataSource {
+    ILog log = BoxLog.getInstance();
+    DruidDataSource datasource;
     public DataSource dataSource() {
-        DruidDataSource datasource = new DruidDataSource();
+        return datasource;
+    }
+
+    @Override
+    @BoxInit // java Box根据此注解进行自动初始化
+    public void init(IConfig config) throws Exception {
+        String url = config.getValue("druidurl");
+        String username = config.getValue("druidusername");
+        String password = config.getValue("druidpassword");
+        String driverClassName = config.getValue("druiddriverClassName");
+        int initialSize = Integer.valueOf(config.getValue("druidinitialSize"));
+        int minIdle = Integer.valueOf(config.getValue("druidminIdle"));
+        int maxActive = Integer.valueOf(config.getValue("druidmaxActive"));
+        int maxWait = Integer.valueOf(config.getValue("druidmaxWait"));
+        int timeBetweenEvictionRunsMillis = Integer.valueOf(config.getValue("druidtimeBetweenEvictionRunsMillis"));
+        int minEvictableIdleTimeMillis = Integer.valueOf(config.getValue("druidminEvictableIdleTimeMillis"));
+        String validationQuery = config.getValue("druidvalidationQuery");
+        boolean testWhileIdle = Boolean.valueOf(config.getValue("druidtestWhileIdle"));
+        boolean testOnBorrow = Boolean.valueOf(config.getValue("druidtestOnBorrow"));
+        boolean testOnReturn = Boolean.valueOf(config.getValue("druidtestOnReturn"));
+        boolean poolPreparedStatements = Boolean.valueOf(config.getValue("druidpoolPreparedStatements"));
+        int maxPoolPreparedStatementPerConnectionSize = Integer.valueOf(config.getValue("druidmaxPoolPreparedStatementPerConnectionSize"));
+        String connectionProperties = config.getValue("druidconnectionProperties");
+
+        datasource = new DruidDataSource();
         datasource.setUrl(url);
         datasource.setUsername(username);
         datasource.setPassword(password);   //这里可以做加密处理
@@ -42,11 +53,6 @@ public class DruidUtil implements IBoxDataSource {
         datasource.setPoolPreparedStatements(poolPreparedStatements);
         datasource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
         datasource.setConnectionProperties(connectionProperties);
-        return datasource;
-    }
-
-    @Override
-    public void init() {
-
+        log.info("阿里云链接池 成功");
     }
 }
