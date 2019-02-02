534f9f4d56568f54fe2b25418c6404d05481bc30
root <lptnii@qq.com>
2019-02-02 14:31:38 +0800

    配置中心

--- java-box-func/src/main/java/com/wzy/func/fc/IBoxDataSource.java
+++ java-box-func/src/main/java/com/wzy/func/fc/IBoxDataSource.java
@@ -1,10 +1,12 @@
 package com.wzy.func.fc;
 
+import org.apache.zookeeper.KeeperException;
+
 import javax.sql.DataSource;
 
 public interface IBoxDataSource {
 
     public DataSource dataSource();
 
-    public void init();
+    public void init(IConfig config) throws Exception;
 }
