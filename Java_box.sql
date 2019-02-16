37cdc15c13433a6ed025dadc2c48ba69462014be
root <lptnii@qq.com>
2019-02-16 18:06:53 +0800

    增加连接池表

--- Java_box.sql
+++ Java_box.sql
@@ -28,13 +28,13 @@ CREATE TABLE IF NOT EXISTS `box_app` (
   `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `jarType` int(11) NOT NULL,
   PRIMARY KEY (`appId`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
 
 -- テーブル java_box.box_app: ~1 rows (approximately) のデータをダンプしています
 DELETE FROM `box_app`;
 /*!40000 ALTER TABLE `box_app` DISABLE KEYS */;
 INSERT INTO `box_app` (`appId`, `name`, `route`, `jarUrl`, `jarName`, `jarMd5`, `stats`, `createTime`, `jarType`) VALUES
-	(13, 'demo', '/demo', '/upload/154865981662140539.jar', 'java-box-dev.jar', '3b9e8e21db6198caa65cd26f39d1eafc', 1, '2019-01-28 15:17:04', 1);
+	(16, 'demo', '/demo', '/upload/155021867192055962.jar', 'java-box-dev.jar', '2d249daf43f85dc11c736cd2ca130a55', 0, '2019-02-15 16:17:54', 1);
 /*!40000 ALTER TABLE `box_app` ENABLE KEYS */;
 
 --  テーブル java_box.box_app_api の構造をダンプしています
@@ -50,13 +50,13 @@ CREATE TABLE IF NOT EXISTS `box_app_api` (
   `runFunction` varchar(50) NOT NULL,
   `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`apiId`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
 
 -- テーブル java_box.box_app_api: ~1 rows (approximately) のデータをダンプしています
 DELETE FROM `box_app_api`;
 /*!40000 ALTER TABLE `box_app_api` DISABLE KEYS */;
 INSERT INTO `box_app_api` (`apiId`, `appId`, `route`, `name`, `linkUrl`, `jarMd5`, `stats`, `runClass`, `runFunction`, `createTime`) VALUES
-	(11, 13, '/demo', 'demoapi4', '/demo/demo', '3b9e8e21db6198caa65cd26f39d1eafc', 0, 'com.demo.UserZdyWork', 'getTest', '2019-01-28 15:17:04');
+	(14, 16, '/demo', 'demoapi4', '/demo/demo', '2d249daf43f85dc11c736cd2ca130a55', 0, 'com.demo.UserZdyWork', 'getTest', '2019-02-15 16:17:54');
 /*!40000 ALTER TABLE `box_app_api` ENABLE KEYS */;
 
 --  テーブル java_box.box_config の構造をダンプしています
@@ -66,20 +66,60 @@ CREATE TABLE IF NOT EXISTS `box_config` (
   `v` varchar(50) NOT NULL,
   `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8mb4 COMMENT='读取配置文件信息';
+) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8mb4 COMMENT='读取配置文件信息';
 
--- テーブル java_box.box_config: ~7 rows (approximately) のデータをダンプしています
+-- テーブル java_box.box_config: ~25 rows (approximately) のデータをダンプしています
 DELETE FROM `box_config`;
 /*!40000 ALTER TABLE `box_config` DISABLE KEYS */;
 INSERT INTO `box_config` (`id`, `k`, `v`, `createTime`) VALUES
-	(46, 'service_api_get_fliters', '/butt/getfliters', '2019-01-30 20:38:30'),
-	(47, 'service_api_get_app_api_list', '/butt/getAppApiList', '2019-01-30 20:38:30'),
-	(48, 'service_api_get_app_list', '/butt/getApplist', '2019-01-30 20:38:30'),
-	(49, 'service_api_get_id', '/butt/getAppId', '2019-01-30 20:38:30'),
-	(50, 'service_api_base_url', 'http://192.168.30.199:9980', '2019-01-30 20:38:30'),
-	(51, 'server_charset', 'utf-8', '2019-01-30 20:38:30'),
-	(52, 'service_apt_down_jar', '/downJar?downUrl=', '2019-01-30 21:05:51');
+	(55, 'service_api_get_fliters', '/butt/getfliters', '2019-02-15 13:44:18'),
+	(56, 'service_api_get_app_api_list', '/butt/getAppApiList', '2019-02-15 13:44:18'),
+	(57, 'service_api_get_app_list', '/butt/getApplist', '2019-02-15 13:44:18'),
+	(58, 'service_api_get_id', '/butt/getAppId', '2019-02-15 13:44:18'),
+	(59, 'service_api_base_url', 'http://localhost:8762', '2019-02-16 16:00:42'),
+	(60, 'server_charset', 'utf-8', '2019-02-15 13:44:18'),
+	(61, 'service_apt_down_jar', '/downJar?downUrl=', '2019-02-15 13:44:18'),
+	(62, 'druidpoolPreparedStatements', '', '2019-02-15 16:15:49'),
+	(63, 'druidpassword', '', '2019-02-15 16:15:59'),
+	(64, 'druidusername', '', '2019-02-15 16:15:59'),
+	(65, 'druidminEvictableIdleTimeMillis', '', '2019-02-15 16:15:59'),
+	(66, 'druidinitialSize', '', '2019-02-15 16:15:59'),
+	(67, 'druidmaxWait', '', '2019-02-15 16:15:59'),
+	(68, 'druidurl', '', '2019-02-15 16:15:59'),
+	(69, 'druiddriverClassName', '', '2019-02-15 16:15:59'),
+	(70, 'druidtimeBetweenEvictionRunsMillis', '', '2019-02-15 16:15:59'),
+	(71, 'druidconnectionProperties', '', '2019-02-15 16:15:59'),
+	(72, 'druidmaxActive', '', '2019-02-15 16:15:59'),
+	(73, 'druidtestOnReturn', '', '2019-02-15 16:15:59'),
+	(74, 'druidmaxPoolPreparedStatementPerConnectionSize', '', '2019-02-15 16:15:59'),
+	(75, 'druidtestOnBorrow', '', '2019-02-15 16:15:59'),
+	(76, 'druidvalidationQuery', '', '2019-02-15 16:15:59'),
+	(77, 'druidminIdle', '', '2019-02-15 16:15:59'),
+	(78, 'druidtestWhileIdle', '123', '2019-02-15 16:16:40'),
+	(79, 'service_api_get_connect_pool', '/butt/getConnectPools', '2019-02-16 17:30:01');
 /*!40000 ALTER TABLE `box_config` ENABLE KEYS */;
+
+--  テーブル java_box.box_connection_pool の構造をダンプしています
+CREATE TABLE IF NOT EXISTS `box_connection_pool` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '0',
+  `className` varchar(50) NOT NULL DEFAULT '0',
+  `methods` varchar(100) NOT NULL DEFAULT '0',
+  `jarName` varchar(100) NOT NULL DEFAULT '0',
+  `jarUrl` varchar(100) NOT NULL DEFAULT '0',
+  `jarMd5` varchar(100) NOT NULL DEFAULT '0',
+  `configStr` varchar(500) NOT NULL DEFAULT '0',
+  `stat` int(11) NOT NULL DEFAULT '0',
+  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COMMENT='Box  连接池插件';
+
+-- テーブル java_box.box_connection_pool: ~1 rows (approximately) のデータをダンプしています
+DELETE FROM `box_connection_pool`;
+/*!40000 ALTER TABLE `box_connection_pool` DISABLE KEYS */;
+INSERT INTO `box_connection_pool` (`id`, `name`, `className`, `methods`, `jarName`, `jarUrl`, `jarMd5`, `configStr`, `stat`, `createTime`) VALUES
+	(4, '', 'com.wzy.plug.db.druid.BoxDruidDataSource', 'init;close;dataSources;dataSource;wait;wait;wait;equals;toString;hashCode;getClass;notify;notifyAll;', 'java-box-plug.jar', '/upload/155030094862617043.jar', '7568b944ca33f9dbbbf099342be3adfd', 'druidurl;druidusername;druidpassword;druiddriverClassName;druidinitialSize;druidminIdle;druidmaxActive;druidmaxWait;druidtimeBetweenEvictionRunsMillis;druidminEvictableIdleTimeMillis;druidvalidationQuery;druidtestWhileIdle;druidtestOnBorrow;druidtestOnReturn;druidpoolPreparedStatements;druidmaxPoolPreparedStatementPerConnectionSize;druidconnectionProperties;', 1, '2019-02-16 17:26:22');
+/*!40000 ALTER TABLE `box_connection_pool` ENABLE KEYS */;
 
 --  テーブル java_box.box_project の構造をダンプしています
 CREATE TABLE IF NOT EXISTS `box_project` (
@@ -130,7 +170,7 @@ CREATE TABLE IF NOT EXISTS `box_user` (
 DELETE FROM `box_user`;
 /*!40000 ALTER TABLE `box_user` DISABLE KEYS */;
 INSERT INTO `box_user` (`user_id`, `user_name`, `user_pass`, `user_stat`, `login_time`, `user_token`, `create_time`) VALUES
-	(1, '123', '202cb962ac59075b964b07152d234b70', 0, '2018-09-28 13:39:52', '2019013018385925596', '2018-09-28 13:39:54');
+	(1, '123', '202cb962ac59075b964b07152d234b70', 0, '2018-09-28 13:39:52', '20190216110635530', '2018-09-28 13:39:54');
 /*!40000 ALTER TABLE `box_user` ENABLE KEYS */;
 
 --  テーブル java_box.box_work_filter の構造をダンプしています
@@ -144,13 +184,13 @@ CREATE TABLE IF NOT EXISTS `box_work_filter` (
   `stat` int(11) NOT NULL DEFAULT '0',
   `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4;
 
 -- テーブル java_box.box_work_filter: ~1 rows (approximately) のデータをダンプしています
 DELETE FROM `box_work_filter`;
 /*!40000 ALTER TABLE `box_work_filter` DISABLE KEYS */;
 INSERT INTO `box_work_filter` (`id`, `jarUrl`, `jarMd5`, `name`, `path`, `className`, `stat`, `createTime`) VALUES
-	(16, '/upload/154865981662140539.jar', '3b9e8e21db6198caa65cd26f39d1eafc', '测试过滤器', '/demo/*', 'com.demo.TestWorkFilter', 0, '2019-01-28 15:17:04');
+	(19, '/upload/155021867192055962.jar', '2d249daf43f85dc11c736cd2ca130a55', '测试过滤器', '/demo/*', 'com.demo.TestWorkFilter', 0, '2019-02-15 16:17:54');
 /*!40000 ALTER TABLE `box_work_filter` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
